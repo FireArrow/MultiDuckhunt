24241b54a20b9c985744bb7df6bb98eb7f4966ee
Peter Hamberg <zazcallabah@gmail.com>
2012-08-03 21:40:42 +0200

    added stuff to simplegame, is now nicer, still not functional

--- js/drawables.js
+++ js/drawables.js
@@ -1,47 +1,100 @@
+var bar = function(name, position, size, color, getPercent ){
+	return {
+		id: name,
+		draw: function( context, width, height, mark, keys ) {
+			context.fillStyle = "white";
+			context.fillRect( position.x, position.y, size.x, size.y );
+			context.fillStyle = color;
+			var colorheight = (size.y - 10) * getPercent();
+			if(colorheight < 0 ) colorheight = 0;
+			context.fillRect( 5+position.x, position.y+(size.y-colorheight)-5, size.x - 10, colorheight  );
+			context.fillStyle = "white";
+			context.font = "12px";
+			context.fillText( name, position.x, position.y + size.y + 10 );
+		}
+	};
+};
+var ending = function( score ) {
+	return {
+		id: "scorescreen",
+		draw: function( context, width, height, mark, keys ) {
+			context.fillStyle = "white";
+			context.font = "bold 80px Consolas, monospace";
+			context.fillText( "GAME OVER", 300, height/2 );
+			context.font = "35px Consolas, monospace";
+			context.fillText( score + " points" , 300, height/2 + 50 );
+		}
+	};
+};
 var enemy = function( killed ){
-	var enemyid = Math.floor( Math.random() * 3 )
+	var enemyid = 0;
 	var imgoffsets = [0,120,250];
 	var imgwidths = [110,120,80];
-	var usedimage = _wSprite;
 	var islive = -1;
 	var angle = 0;
 	var state = [0,90];
 	var _size = 100;
-	var position = vec({x:40,y:50,z:300});
-	var animateLive = function( context, x, y, s )
-	{
+	var position = vec();
+	var velocity = vec();
+	var reset = function(){
+		enemyid = Math.floor( Math.random() * 3 );
+		islive = -1;
+		_size = 100 + (Math.random()-0.5)*30;
+		position = vec({x:(Math.random()-0.5)*400,
+						y:(Math.random()-0.5)*400,
+						z:Math.random()*100 + 10000});
+		velocity = vec({x:(Math.random()-0.5)*0.7,
+						y:(Math.random()-0.5)*0.7,
+						z:(-1)*(4+Math.random())});
+		};
+	reset();
 
-		context.drawImage(_wSprite, imgoffsets[enemyid], state[0], imgwidths[enemyid],state[1], x,y,s,s);
-	};
-	var animateDeath = function( context, x, y, s )
-	{
-
-		context.drawImage(_rSprite, imgoffsets[enemyid], state[0], imgwidths[enemyid],state[1], x,y,s,s);
-	};
 	return {
 		id:"enemy",
 		pos: function(){ return position;},
 		size: function(){ return _size; },
 		tick: function( keys, mark, delta ) {
-			if(keys.length > 0 )
+			if(keys.length > 0 && islive === -1 )
 			{
 				islive = mark;
-			}
-			state = ( Math.floor( mark / 1000 ) % 2 == 0) ? [0,90] : [80,90];
-			position = position.add(
-				vec({x:Math.random()*.01,y:Math.random*0.01,z:-0.1}).mul( delta )
-		);
+				velocity = vec({
+					x: velocity.x()*10,
+					y: velocity.y()*10,
+					z: velocity.z()/10
+				});
 
+			}
+			if( islive !== -1 && mark-islive > 2000 || position.z() < -10000 )
+			{
+				reset();
+			}
+
+			state = ( Math.floor( mark / 1000 ) % 2 == 0) ? [0,90] : [80,90];
+			position = position.add( velocity.mul( delta ) );
 		},
 		draw: function( context, x, y, s ) {
-
-			if( islive == -1 )
-				animateLive( context, x, y, s );
-			else
+			if( _debug !== undefined )
 			{
-				animateDeath(context, x, y, s);
+				context.fillStyle = "rgba(0,255,0,100)";
+				context.beginPath();
+				context.arc( x, y, s/2, 0, Math.PI*2, true);
+				context.fill();
 			}
 
+			if( islive == -1 )
+			{
+				context.setTransform(1, 0, 0, 1, -s/2, -s/2);
+				context.drawImage(_wSprite, imgoffsets[enemyid], state[0], imgwidths[enemyid],state[1], x,y,s,s);
+			}
+			else
+			{
+
+				angle+=0.1;
+				var sin = Math.sin(angle);
+				var cos = Math.cos(angle);
+				context.setTransform( cos, sin, -sin, cos, x, y );
+				context.drawImage(_rSprite, imgoffsets[enemyid], state[0], imgwidths[enemyid],state[1], -s/2,-s/2,s,s);
+			}
 		}
 	};
 };
--- js/state.js
+++ js/state.js
@@ -1,20 +1,21 @@
 var makeServerState = function()
 {
 	var _current_server_state = [];
-	
+	var _remaining_ammo = 0;
 	(function w_req(){
 		$.ajax({
 			url: "api/current",
 			dataType:"json",
 			success: function(data)
 			{
-				if(data instanceof Array)
+				if(data instanceof Array )
 				{
 					var points_arr = [];
-					for( var i = 0; i<data.length;i++ )
+					for( var i = 0; i<data.length && _remaining_ammo > 0 ;i++ )
 					{
 						var lst = data[i].split(",");
 						points_arr.push( {x:parseFloat(lst[0]),y:parseFloat(lst[1]) } );
+						_remaining_ammo-=4;
 					}
 					_current_server_state = points_arr;
 				}
@@ -34,6 +35,9 @@ var makeServerState = function()
 	})();
 
 	return {
-		getState: function() { return _current_server_state; }
+		getState: function() { return _current_server_state; },
+		getAmmo: function() { return _remaining_ammo; },
+		getMaxAmmo: function() { return 1000; },
+		reload: function() { _remaining_ammo = this.getMaxAmmo(); }
 	};
 };
