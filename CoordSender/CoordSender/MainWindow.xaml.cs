98c6cbe3a9365b43ce4a633dc1927020e62f7cf5
Peter Hamberg <zazcallabah@gmail.com>
2012-08-06 10:39:22 +0200

    added small debug app that sends mouse coordinates using udp

---
+++ CoordSender/CoordSender/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Input;
+namespace CoordSender
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		bool _keep;
+		readonly Sender _s;
+		public MainWindow()
+		{
+			InitializeComponent();
+			_s = new Sender();
+			DataContext = _s;
+		}
+
+		private void Window_MouseDown( object sender, MouseButtonEventArgs e )
+		{
+			var coords = System.Windows.Forms.Control.MousePosition;
+			var stringdata = string.Format( "{0},{1}", coords.X, coords.Y );
+			if( _keep )
+			{
+				if( _s.DataSource.Equals( " " ) )
+				{
+					_s.DataSource = stringdata;
+				}
+				else
+					_s.DataSource = _s.DataSource + " " + stringdata;
+			}
+			else
+			{
+				_s.DataSource = stringdata;
+			}
+		}
+
+		private void Window_MouseUp( object sender, MouseButtonEventArgs e )
+		{
+			if( !_keep )
+			{
+				_s.DataSource = " ";
+			}
+		}
+
+		private void Window_KeyDown( object sender, KeyEventArgs e )
+		{
+			if( _keep )
+				_s.DataSource = " ";
+			_keep = !_keep;
+		}
+
+		private void Window_KeyUp( object sender, KeyEventArgs e )
+		{
+		}
+	}
+}
---
+++ CoordSender/CoordSender/Sender.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Timers;
+
+namespace CoordSender
+{
+	public class Sender : INotifyPropertyChanged
+	{
+		Socket _socket;
+		IPAddress _serverAddr;
+		IPEndPoint _endPoint;
+
+		public Sender()
+		{
+			_socket = new Socket( AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp );
+			_serverAddr = IPAddress.Parse( ConfigurationManager.AppSettings["TargetServer"] ?? "127.0.0.1" );
+			_endPoint = new IPEndPoint( _serverAddr, Int32.Parse( ConfigurationManager.AppSettings["TargetPort"] ?? "10001" ) );
+			_data = "";
+			var t = new Timer( Double.Parse( ConfigurationManager.AppSettings["SendingInterval"] ?? "100" ) );
+			t.Elapsed += Send;
+			t.Enabled = true;
+		}
+
+		void Send( object sender, ElapsedEventArgs e )
+		{
+			byte[] send_buffer = Encoding.UTF8.GetBytes( DataSource );
+
+			_socket.SendTo( send_buffer, _endPoint );
+		}
+
+		string _data;
+
+		public string DataSource
+		{
+			get { return _data; }
+			set
+			{
+				_data = value;
+				OnPropertyChanged( new PropertyChangedEventArgs( "DataSource" ) );
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged( PropertyChangedEventArgs e )
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if( handler != null )
+				handler( this, e );
+		}
+	}
+}
