070ed04e63a9c51e87957614801971ef1adc7340
Peter Hamberg <zazcallabah@gmail.com>
2012-08-06 12:47:45 +0200

    added some console logging to nodeserver

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,7 +1,7 @@
 var udp_listen_port = 10001;
-
+var debug_mode = false;
 // latest state of detected light (format: 0.5,22.388 33.21,9993 ...)
-var current_state = "0,0";
+var current_state = "";
 
 // Listen for udp
 var dgram = require("dgram");
@@ -9,10 +9,12 @@ var server = dgram.createSocket("udp4");
 server.on( "listening",
 	function () {
 		var address = server.address();
+		if(debug_mode)
 		console.log("listening for udp datagrams on " + address.address + ":" + address.port);
 });
 server.on("message",
 	function (msg, rinfo) {
+	if(debug_mode)
 		console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
 		current_state = ""+msg;
 });
@@ -40,10 +42,12 @@ var requesthandler = function( request, response ) {
 	}
 	else
 	{
+		
 		var uri = "../js" + pathname;
 		var filename = path.join(process.cwd(), uri);
 		path.exists(filename, function(exists) {
 			if(!exists) {
+			console.log("404: "+pathname);
 				response.writeHead(404, {"Content-Type": "text/plain"});
 				response.write("404 Not Found\n");
 				response.end();
@@ -55,11 +59,13 @@ var requesthandler = function( request, response ) {
 
 			fs.readFile(filename, "binary", function(err, file) {
 				if(err) {        
+					console.log("500: "+pathname);
 					response.writeHead(500, {"Content-Type": "text/plain"});
 					response.write(err + "\n");
 					response.end();
 					return;
 				}
+				console.log("200: "+pathname);
 				response.writeHead(200);
 				response.write(file, "binary");
 				response.end();
