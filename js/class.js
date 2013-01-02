048feee650379143a6361cf415ae7305a2bde512
Peter Hamberg <zazcallabah@gmail.com>
2013-01-02 23:44:44 +0100

    switched to prototype based vec class

--- js/class.js
+++ js/class.js
@@ -1,5 +1,8 @@
 // When coding in javascript, we dont use no new keyword for objects. We just create them.
-// Follows: Crockfords pattern for object creation. Is good.
+// Follows: Crockfords pattern for closured object creation. Is good.
+
+// IMPORTANT: Value objects, objects that will exists in more than one or two instances at the same time
+// should have its functions declared on the prototype for performance reasons, see below.
 
 var makeClass = function( param1, param2 )
 {
@@ -19,4 +22,29 @@ var makeClass = function( param1, param2 )
 		publicMethod: function(str){ field2=str; },
 		properGetter: function(){ return field2; }
 	};
+};
+
+function Particle( param1 )
+{
+	this.color = param1.color;
+	this.spin = param1.spin;
+	this._ugly_pseudo_private_internal = 100;
+};
+
+Particle.prototype.act = function(){
+	if( this._ugly_pseudo_private_internal-- > 20 )
+		return "not yet";
+	else
+	{
+		this._ugly_pseudo_private_internal = 100;
+		return this.color;
+	}
+};
+
+// Fake accessors
+Particle.prototype.getSpin = function(){
+	return this.spin;
+};
+Particle.prototype.setSpin = function(s){
+	this.spin = s;
 };
--- js/vec.js
+++ js/vec.js
@@ -1,71 +1,66 @@
 /*
-* A three dimensional vector.
-* Not modifiable, if you want to change a vector, make a new one.
-*/
-var vec = function vec( par )
+ * A three dimensional vector.
+ * Not modifiable, if you want to change a vector, make a new one.
+ */
+function Vec( par )
 {
-	if( par === undefined )
-		par = {};
-	
-	// fields
-	var _x = par.x || 0;
-	var _y = par.y || 0;
-	var _z = par.z || 0;
-	
-	// This is for pretty-printing
-	var num = function( i ) {
-		i *= 1000;
-		i = Math.round(i);
-		i /= 1000;
-		return i;
-	};
-
-	// cache the length of this vector, but lazy-load it as well
-	var abs = undefined; 
-
-	return {
-		x: function(){return _x;},
-		y: function(){return _y;},
-		z: function(){return _z;},
-		
-		// return length of this vector
-		abs: function()
-		{
-			if( abs === undefined ) { abs = Math.sqrt( _x*_x + _y*_y + _z*_z ); }
-			return abs;
-		},
-		
-		// return the unit vector pointing in the same direction as this vector.
-		unit: function(){ return this.mul( 1 / this.abs() ); }, 
-		norm: function(){ return this.unit(); },
-		
-		// return the dot product of this and v
-		dot: function( v ){ return _x * v.x() + _y * v.y() + _z * v.z(); }, 
-		
-		// return the cross product of this and v
-		cross: function( v ) { return vec({
-			x: _y * v.z() - _z * v.y(),
-			y: _z * v.x() - _x * v.z(),
-			z: _x * v.y() - _y * v.x()
-		});},
-		
-		// return the product of the vector 'this' and the parameter scalar
-		mul: function( scal ) { return vec( {x:scal*_x,y:scal*_y,z:scal*_z} ); },
-		
-		add: function( v ) {
-			return vec({
-				x: _x + v.x(),
-				y: _y + v.y(),
-				z: _z + v.z()
-			});
-		},
-		sub: function( v ) {return vec({
-			x: _x - v.x(),
-			y: _y - v.y(),
-			z: _z - v.z()
-		}); },
-		
-		// printable string representing this vector
-		info: function() { return "[ "+num(_x)+", "+num(_y)+", "+num(_z)+" ]";}
-	};
+	this._abs = undefined;
+	this.data = par || [0,0,0];
 };
+
+Vec.prototype.num = function(i){
+	i *= 1000;
+	i = Math.round(i);
+	i /= 1000;
+	return i;
+};
+
+Vec.prototype.abs = function(){
+	if( this._abs === undefined )
+		this._abs = Math.sqrt( this.data[0]*this.data[0] + this.data[1]*this.data[1] + this.data[2]*this.data[2] );
+	return this._abs;
+};
+
+Vec.prototype.x = function(){return this.data[0];};
+Vec.prototype.y = function(){return this.data[1];};
+Vec.prototype.z = function(){return this.data[2];};
+
+// return the unit vector pointing in the same direction as this vector.
+Vec.prototype.unit = function(){ return this.mul( 1 / this.abs() ); };
+Vec.prototype.norm = function(){ return this.unit(); };
+
+// return the dot product of this and v
+Vec.prototype.dot = function( v ){ return this.data[0] * v.data[0] + this.data[1] * v.data[1] + this.data[2] * v.data[2]; };
+
+// return the cross product of this and v
+Vec.prototype.cross = function( v ){
+	return new Vec([
+		this.data[1] * v.data[2] - this.data[2] * v.data[1],
+		this.data[2] * v.data[0] - this.data[0] * v.data[2],
+		this.data[0] * v.data[1] - this.data[1] * v.data[0]
+	]);
+};
+
+// return the product of the vector 'this' and the parameter scalar
+Vec.prototype.mul = function( scal ) { return new Vec( [scal*this.data[0], scal*this.data[1], scal*this.data[2]] ); };
+
+Vec.prototype.add = function( v ) {
+	return new Vec([
+		this.data[0] + v.data[0],
+		this.data[1] + v.data[1],
+		this.data[2] + v.data[2]
+	]);
+};
+
+Vec.prototype.sub = function( v ) {
+	return new Vec([
+		this.data[0] - v.data[0],
+		this.data[1] - v.data[1],
+		this.data[2] - v.data[2]
+	]);
+};
+
+// printable string representing this vector
+Vec.prototype.info = function() { return "[ "+this.num(this.x())+", "+this.num(this.y())+", "+this.num(this.z())+" ]";};
+Vec.prototype.toString = function() { return "tostr"+this.info()};
+Vec.prototype.toSource = function() { return "tosou"+this.info()};
